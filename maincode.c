9d9be1710809814f49e67411f9d46bdd29cfc885
utkarsh04v <166329013+utkarsh04v@users.noreply.github.com>
2024-04-08 01:34:18 +0530

    Create maincode.c

---
+++ maincode.c
@@ -0,0 +1,16 @@
+//Group No. 33 - CS103 project
+//Members: 
+//Utkarsh Verma 2301MC32
+//Karan Veer Gautam Sharma	2301MC10
+//Hemanth Venkata Sai Jonnadula	2301ME20
+//Lalit Thakur	2301ME27
+//Masthan Vali Shaik	2301MM09
+//Anshul Verma	2301MM37	
+//Sushanth Yelishetty	2302MT04
+//Vishnu Vardhan Ramavath	2302GT04
+//Vishnu Vardhini Allam    2301AI35
+//Varada Chirag Patel 2301MC38
+
+int main(){
+  /*      :)     */
+}
